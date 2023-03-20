d854e4a9ea0e0efd6c20af9b4acc721476795b52
Flo <florian.schildwaechter@gmail.com>
2023-03-20 21:46:16 +0100

    initial setup

---
+++ configs/overrides.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "typescript",
+    "tsx",
+    "json",
+    "jsdoc",
+    "glimmer",
+  },
+  indent = {
+    enable = true,
+    disable = {
+      "python",
+    },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "emmet-ls",
+    "json-lsp",
+    "prettier",
+
+    -- shell
+    "shellcheck",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+    ignore = false,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
